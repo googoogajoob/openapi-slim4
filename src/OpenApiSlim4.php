cff3cb2e53582a62bbc9d89027b92111890b6a8f
APC <apc@andypotter.org>
2021-07-08 12:23:30 +0200

    still setting things up 2.0

---
+++ src/OpenApiSlim4.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Project openapi-slim4
+ * Created by PhpStorm.
+ * User: and
+ * Date: 08.07.21
+ * Time: 12:08
+ */
+
+namespace googoogajoob\openapislim4;
+
+class OpenApiSlim4
+{
+    public function junk()
+    {
+        return "junk";
+    }
+}
