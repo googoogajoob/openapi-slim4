c78eb5a4b72634736d86786588d367ae8a0bd33a
APC <apc@andypotter.org>
2022-10-24 04:00:49 +0200

    testing server

---
+++ tests/docker-test-environment/src/TestService.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace TestServer;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Render
+{
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        $returnStatus = 501;
+        $payload = ['data' => 'Test Response'];
+        $response = $response->withHeader('Content-type', 'application/json');
+        $response = $response->withStatus($returnStatus);
+        $response->getBody()->write(json_encode($payload));
+
+        return $response;
+    }
+}
