4a8c6baddb76e203c26eb1a6fc32888821a54131
APC <apc@andypotter.org>
2022-10-28 20:34:57 +0200

    Defining openapi

--- development/docker-test-environment/bin/startup-dev.sh
+++ development/docker-test-environment/bin/startup-dev.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#
+
+echo "startup-dev.sh executing"
 cd /var/www
+export XDEBUG_MODE=off
 composer install
-apache2 -D FOREGROUND
+unset XDEBUG_MODE
+cd /var/www/bin
+
+#start apache
+apache2-foreground
