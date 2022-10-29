a27bf361e48c247e77ee5dc6cfdab16101136599
APC <apc@andypotter.org>
2022-10-29 23:02:12 +0200

    Index php is working

--- development/docker-test-environment/src/TestService.php
+++ development/docker-test-environment/src/TestService.php
@@ -6,7 +6,7 @@ namespace TestServer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class TestEndpoint
+class TestService
 {
     /**
      * @param Request $request
