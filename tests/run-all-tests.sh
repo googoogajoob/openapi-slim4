eed1e1997e027b0534aebf4c49cde35c40b41eaf
APC <apc@andypotter.org>
2022-11-09 21:42:49 +0100

    (First time) This is actually quite awesome :)

--- tests/run-all-tests.sh
+++ tests/run-all-tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec docker-test-environment_slim4-test_1 /usr/local/bin/php /var/www/vendor/bin/codecept --report run api
+docker exec --env NATIVE_SLIM_CONFIG=0 docker-test-environment_slim4-test_1 /usr/local/bin/php /var/www/vendor/bin/codecept --report run api
+docker exec --env NATIVE_SLIM_CONFIG=1 docker-test-environment_slim4-test_1 /usr/local/bin/php /var/www/vendor/bin/codecept --report run api
